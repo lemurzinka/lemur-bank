55809ed12e9cafdbc83b4e4ec2ee0b3b83f92c31
lemur <denisgevkach@gmail.com>
2025-01-26 16:48:54 +0200

    final fix

--- src/test/java/tests/ValidationServiceTest.java
+++ src/test/java/tests/ValidationServiceTest.java
@@ -4,6 +4,7 @@ import botBank.service.ValidationService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
